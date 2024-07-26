443013322dd5afdc1daff1015a8458fbfd870a34
Vadimbuildercxx <kurvadimal@gmail.com>
2024-07-26 12:51:00 +0400

    Update train.py

--- scripts/train_for_test.py
+++ scripts/train_for_test.py
@@ -18,7 +18,6 @@ import wandb
 
 torch.backends.cudnn.benchmark = True
 
-
 def validate_model(
         model,
         n_loops,
@@ -52,7 +51,7 @@ def validate_model(
             if family == 'gpt2':
                 output = model(xs, ys)  # [B,]
             elif family == 'gpt2_loop':
-                n_loops = n_loops  # curriculum.n_loops  # K
+                n_loops = n_loops # curriculum.n_loops  # K
                 y_pred_list = model(xs, ys, 0, n_loops)
                 output = y_pred_list[-1]  # [B, n]
             else:
@@ -62,7 +61,6 @@ def validate_model(
             val_loss += loss.item()
     val_loss /= len(val_loader)
     return val_loss
-
 
 def calculate_gradient_norm(model):
     total_norm = 0.0
@@ -75,22 +73,22 @@ def calculate_gradient_norm(model):
     return norm_dict, total_norm
 
 
-def train_step(curriculum, model, xs, ys, optimizer, ctx, scaler, add_inputs_embeds, use_ctx, n_loop_window, family):
-    if family in ['gpt2', 'gpt2_tying']:
+def train_step(args, curriculum, model, xs, ys, optimizer, ctx, scaler):
+    if args.model.family in ['gpt2', 'gpt2_tying']:
         if ctx is not None:
             with ctx:
-                y_pred = model(xs, ys, add_inputs_embeds=add_inputs_embeds)  # [B, n]
+                y_pred = model(xs, ys, add_inputs_embeds=args.training.add_inputs_embeds)  # [B, n]
                 # list of [B, n], length K + 1, get rid of the 0-th one
                 loss = (ys - y_pred).square().mean()  # auto on both K and n (number of in context samples)
         else:
-            y_pred = model(xs, ys, add_inputs_embeds=add_inputs_embeds)  # [B, n]
+            y_pred = model(xs, ys, add_inputs_embeds=args.training.add_inputs_embeds)  # [B, n]
             # list of [B, n], length K + 1, get rid of the 0-th one
             loss = (ys - y_pred).square().mean()  # auto on both K and n (number of in context samples)
-    elif family in ['gpt2_loop']:
+    elif args.model.family in ['gpt2_loop']:
         n_loops = curriculum.n_loops  # K
         if ctx is not None:
             with ctx:
-                horizon_start = max(0, n_loops - n_loop_window)
+                horizon_start = max(0, n_loops - args.training.n_loop_window)
                 y_pred_list = model(xs, ys, horizon_start, n_loops)
                 # list of [B, n], length K
                 y_pred_arr = torch.cat(y_pred_list, dim=0)  # [B * K, n]
@@ -98,14 +96,14 @@ def train_step(curriculum, model, xs, ys, optimizer, ctx, scaler, add_inputs_emb
                 loss = (y_star_arr - y_pred_arr).square().mean()  # auto on both K and n (number of in context samples)
                 y_pred = y_pred_list[-1]  # [B, n]
         else:
-            horizon_start = max(0, n_loops - n_loop_window)
+            horizon_start = max(0, n_loops - args.training.n_loop_window)
             y_pred_list = model(xs, ys, horizon_start, n_loops)
             # list of [B, n], length K
             y_pred_arr = torch.cat(y_pred_list, dim=0)  # [B * K, n]
             y_star_arr = torch.cat([ys] * len(y_pred_list), dim=0)  # [B * K, n]
             loss = (y_star_arr - y_pred_arr).square().mean()  # auto on both K and n (number of in context samples)
             y_pred = y_pred_list[-1]  # [B, n]
-    if use_ctx:
+    if args.training.use_ctx:
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
@@ -117,138 +115,140 @@ def train_step(curriculum, model, xs, ys, optimizer, ctx, scaler, add_inputs_emb
     return loss.detach(), y_pred.detach(), total_norm, norm_dict
 
 
-def train_loop(args, model, curriculum, device, do_wandb_log):
-    """Method for training loop from configuration."""
-    return train_without_config(
-        model, curriculum, lr=args.training.learning_rate,
-        add_inputs_embeds=args.training.add_inputs_embeds, task_name=args.training.task_name,
-        batch_size=args.training.batch_size, n_loop_window=args.training.n_loop_window,
-        model_n_dims=args.model.n_dims, train_steps=args.training.train_steps,
-        family=args.model.family, experiment_name=args.wandb.name,
-        out_dir=args.out_dir, do_wandb_log=do_wandb_log, log_every_steps=args.wandb.log_every_steps,
-        use_ctx=args.training.use_ctx,
-        project_name=args.wandb.project, project_notes=args.wandb.notes,
-        seed=args.training.seed, weight_decay=args.training.weight_decay,
-        sparsity=args.training.sparsity, save_every_steps=args.training.save_every_steps,
-        device=device)
-
-
-def train_without_config(model,
-                         curriculum,
-                         lr=0.0001,
-                         add_inputs_embeds = False,
-                         task_name="linear_regression",
-                         batch_size=64,
-                         n_loop_window=20,
-                         model_n_dims=10,
-                         train_steps=10000,
-                         family="gpt2",
-                         experiment_name="linear_regression_gpt_2",
-                         out_dir="./results2/linear_regression_baseline",
-                         do_wandb_log=False,
-                         log_every_steps=100,
-                         use_ctx=False,
-                         project_name="base_project",
-                         project_notes="",
-                         seed=42,
-                         weight_decay=0.0,
-                         sparsity=False, save_every_steps=1000, device="cuda",):
+def main(args, device):
     # TORCH 2.0 ZONE ###############################
-    metrics = []
     torch.set_float32_matmul_precision('highest')
     torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
     torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
     dtype = 'float16'  # 'bfloat16', 'float32'
     ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-    if use_ctx:
+    if args.training.use_ctx:
         ctx = torch.amp.autocast(device_type='cuda', dtype=ptdtype, cache_enabled=False)
     else:
         ctx = None
+    #ctx = torch.amp.autocast(device_type='cuda', dtype=ptdtype, cache_enabled=False)
     ################################################
+    wandb.init(
+        dir=args.out_dir,
+        project=args.wandb.project,
+        config=args.__dict__,
+        notes=args.wandb.notes,
+        name=args.wandb.name,
+        mode="disabled" if args.debug_mode else "online",
+        resume=True,
+    )
 
-    if do_wandb_log:
-        wandb.init(
-            dir=out_dir,
-            project=project_name,
-            #config=args.__dict__,
-            notes=project_notes,
-            name=experiment_name,
-            #mode="disabled" if args.debug_mode else "online",
-            resume=True,
-        )
-
-    torch.manual_seed(seed)
-
+    torch.manual_seed(args.training.seed)
+    model = build_model(args.model)
     # model = torch.compile(model)
+
     model.to(device)
     model.train()
 
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=lr, weight_decay=weight_decay)
+        model.parameters(), lr=args.training.learning_rate, weight_decay=args.training.weight_decay)
     scaler = torch.cuda.amp.GradScaler(enabled=(dtype == 'float16'))
-
+    curriculum = Curriculum(args.training.curriculum)
 
     # Here the model load the pretrained model
-    # args, model, optimizer, curriculum, state_path, starting_step = load_pretrained_model(
-    #     args, model, optimizer, curriculum, device)
+    args, model, optimizer, curriculum, state_path, starting_step = load_pretrained_model(
+        args, model, optimizer, curriculum, device)
 
-    pbar = tqdm(range(0, train_steps))
-    for i in pbar:
+    if args.training.use_fixed_dataset:
+
         task_sampler = get_task_sampler(
-            task_name=task_name,
-            batch_size=batch_size,
+            task_name=args.training.task_name,
+            batch_size=args.training.batch_size,
             n_points=curriculum.n_points,
-            n_dims=model_n_dims,
+            n_dims=args.model.n_dims,
             n_dims_truncated=curriculum.n_dims_truncated,
             device=device,
-            sparsity=sparsity,
+            sparsity=args.training.sparsity,
         )
+        train_loader = gen_dataloader(task_sampler, args.training.train_size,
+                                      args.training.batch_size)
+        train_iter = iter(train_loader)
+        test_loader = gen_dataloader(task_sampler, args.training.test_size,
+                                     args.training.batch_size)
 
-        real_task = task_sampler()
-        xs, ys = real_task.xs.float(), real_task.ys.float()
+    pbar = tqdm(range(starting_step, args.training.train_steps))
+    for i in pbar:
+        if args.training.use_fixed_dataset:
+            try:
+                batch = next(train_iter)
+                xs, ys = batch['x'].to(device), batch['y'].to(device)
+            except StopIteration:
+                train_iter = iter(train_loader)
+        else:
+            task_sampler = get_task_sampler(
+                task_name=args.training.task_name,
+                batch_size=args.training.batch_size,
+                n_points=curriculum.n_points,
+                n_dims=args.model.n_dims,
+                n_dims_truncated=curriculum.n_dims_truncated,
+                device=device,
+                sparsity=args.training.sparsity,
+            )
 
-        #loss, output, total_norm, grad_norm_dict = train_step(curriculum, model, xs, ys, optimizer, ctx, scaler)
-        loss, output, total_norm, grad_norm_dict = train_step(curriculum, model, xs, ys, optimizer, ctx, scaler,
-                                                              add_inputs_embeds, family=family, use_ctx=use_ctx, n_loop_window=n_loop_window)
+            real_task = task_sampler()
+            xs, ys = real_task.xs.float(), real_task.ys.float()
+
+        loss, output, total_norm, grad_norm_dict = train_step(args, curriculum, model, xs, ys, optimizer, ctx, scaler)
 
         # EVALUATION ======================================
         point_wise_tags = list(range(curriculum.n_points))  # [0, 1, 2, ..., n-1]
-        if i % log_every_steps == 0:
+        if i % args.wandb.log_every_steps == 0:
             point_wise_loss = (output - ys).square().mean(dim=0)  # [n,]
-            epoch_metrics_dict = {
-                "scaled_loss": loss.item() / curriculum.n_dims_truncated,
-                "overall_loss": loss.item(),
-                "loop_times": curriculum.n_loops,
-                "grad_norm/layerwise": grad_norm_dict,
-                "grad_norm": total_norm,
-                "pointwise/loss": dict(
-                    zip(point_wise_tags, point_wise_loss.detach().cpu().numpy())
-                ),
-                "n_points": curriculum.n_points,
-                "n_dims": curriculum.n_dims_truncated,
-                "lr": optimizer.param_groups[0]['lr'],
-            }
-
-            if do_wandb_log:
-                wandb.log(epoch_metrics_dict, step=i)
-
-            # Save metrics to wandb and out metrics array
-            epoch_metrics_dict["step"] = i
-            metrics.append(epoch_metrics_dict)
+            if args.training.use_fixed_dataset:
+                # eval
+                with torch.no_grad():
+                    for batch in test_loader:
+                        xs, ys = batch['x'].to(device), batch['y'].to(device)
+                        if args.model.family in ['gpt2']:
+                            output = model(xs, ys)  # [B,]
+                        elif args.model.family in ['gpt2_loop']:
+                            n_loops = curriculum.n_loops  # K
+                            y_pred_list = model(xs, ys, 0, n_loops)
+                            output = y_pred_list[-1]  # [B, n]
+                        else:
+                            raise NotImplementedError
+                        point_wise_loss = (output - ys).square().mean(dim=0)
+                        loss = point_wise_loss.mean()
+            wandb.log(
+                {
+                    "scaled_loss": loss / curriculum.n_dims_truncated,
+                    "overall_loss": loss,
+                    "loop_times": curriculum.n_loops,
+                    "grad_norm/layerwise": grad_norm_dict,
+                    "grad_norm": total_norm,
+                    "pointwise/loss": dict(
+                        zip(point_wise_tags, point_wise_loss.detach().cpu().numpy())
+                    ),
+                    "n_points": curriculum.n_points,
+                    "n_dims": curriculum.n_dims_truncated,
+                    "lr": optimizer.param_groups[0]['lr'],
+                },
+                step=i,
+            )
 
         curriculum.update()
 
         pbar.set_description(f"loss {loss}")
-        if i % save_every_steps == 0:
+        if i % args.training.save_every_steps == 0:
             training_state = {
                 "model_state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict(),
                 "train_step": i,
             }
-            if not os.path.exists(out_dir):
-                os.makedirs(out_dir)
-            torch.save(training_state, os.path.join(out_dir, f"model_{i}.pt"))
-    return metrics
+            torch.save(training_state, state_path)
+        if (
+                args.training.keep_every_steps > 0
+                and i % args.training.keep_every_steps == 0
+                and i > 0
+        ) or (i == args.training.train_steps - 1):
+            torch.save({'model': model.state_dict()},
+                       os.path.join(args.out_dir, f"model_{i}.pt"))
+
 
 if __name__ == "__main__":
     parser = QuinineArgumentParser(schema=schema)
@@ -274,8 +274,4 @@ if __name__ == "__main__":
     with open(os.path.join(out_dir, "config.yaml"), "w") as yaml_file:
         yaml.dump(args.__dict__, yaml_file, default_flow_style=False)
 
-    model = build_model(args.model)
-
-    curriculum = Curriculum(args.training.curriculum)
-    ####
-    train_loop(args, model, curriculum, device, do_wandb_log=True)
+    main(args, device)
