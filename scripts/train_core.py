5cd285ae70a6828d40516ff0dda6127b136e4b0e
Vadimbuildercxx <kurvadimal@gmail.com>
2024-07-30 16:59:29 +0400

    Update.

--- scripts/train_core.py
+++ scripts/train_core.py
@@ -130,7 +130,10 @@ def train_without_config(model,
                          sparsity=False, save_every_steps=1000, device="cuda",):
     # TORCH 2.0 ZONE ###############################
     metrics = []
-    torch.set_float32_matmul_precision('highest')
+
+    if hasattr(torch, "set_float32_matmul_precision"):
+        torch.set_float32_matmul_precision('highest')
+
     torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
     torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
     dtype = 'float16'  # 'bfloat16', 'float32'
