8727467df9b92f1b61abf3a45a960dabed072fdc
Vadimbuildercxx <kurvadimal@gmail.com>
2024-08-03 22:43:44 +0400

    Fix

--- scripts/nano_mamba.py
+++ scripts/nano_mamba.py
@@ -9,7 +9,8 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from mamba_block import MambaBlock
+#from mamba_block import MambaBlock
+from einops import rearrange, repeat, einsum
 
 # @torch.jit.script # good to enable when not using torch.compile, disable when using (our default)
 def new_gelu(x):
@@ -38,126 +39,361 @@ class MambaConfig:
     vocab_size: int = 50304  # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 5 # Mamba depth
     dt_rank: int = 16
-    dim_inner: int = 256
+    expand: int = 2
     d_state: int = 16
     d_conv: int = 4
-    n_embd: int = 768
+    n_embd: int = 256
     img_dim: int = 64
+    conv_bias: bool = True
     bias: bool = True  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+
+    def __post_init__(self):
+        self.d_inner = int(self.expand * self.n_embd)
 
 
 class Mamba(nn.Module):
-    """Mamba model.
-
-    Args:
-        vocab_size (int): The size of the vocabulary.
-        dim (int): The input dimension.
-        depth (int): The depth of the Mamba block.
-        d_state (int): The state dimension. Default is 16.
-        expand (int): The expansion factor. Default is 2.
-        dt_rank (Union[int, str]): The rank of the temporal difference (Δ) tensor. Default is "auto".
-        d_conv (int): The dimension of the convolutional kernel. Default is 4.
-
-    Examples:
-    x = torch.randint(0, 16, (1, 64))
-    model = Mamba(16, 64, 5, 16)
-    out = model(x)
-    print(out)
-    """
-
-    def __init__(
-            self,
-            config,
-    ):
+    def __init__(self, config: MambaConfig):
         """Full Mamba model."""
-        super(Mamba, self).__init__()
-        assert config.vocab_size is not None
-        assert config.block_size is not None
+        super().__init__()
+        self.config = config
 
+        self.layers = nn.ModuleList([ResidualBlock(config) for _ in range(config.n_layer)])
         self.norm_f = RMSNorm(config.n_embd)
-        self.mamba_layers = nn.ModuleList(
-            [
-                torch.nn.Sequential(
-                    MambaBlock(dim=config.n_embd, depth=config.n_layer, d_state=config.d_state, d_conv=config.d_conv),
-                    RMSNorm(config.n_embd)
-                )
-                for _ in range(config.n_layer)
-            ]
-        )
-        # Projection for img
-        self.img_proj = nn.Linear(config.img_dim, config.n_embd)
-        print("number of parameters: %.2fM" % (self.get_num_params() / 1e6,))
 
-    def get_num_params(self, non_embedding=True):
-        """
-        Return the number of parameters in the model.
-        """
-        n_params = sum(p.numel() for p in self.parameters())
-        return n_params
-
-    def forward(self, x: torch.Tensor, context: torch.Tensor = None):
+    def forward(self, input_ids):
         """
         Args:
-            x (float tensor): input tensor
+            input_ids (long tensor): shape (b, l)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+        Returns:
+            logits: shape (b, l, vocab_size)
+
+        Official Implementation:
+            class MambaLMHeadModel, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/models/mixer_seq_simple.py#L173
 
         """
+        x = self.embedding(input_ids)
 
-        if exists(context):
-            # Project the image
-            projected_img = self.img_proj(context)
-
-            # Concatenate the image and text
-            x = torch.cat([x, projected_img], dim=1)
-
-        for layer in self.mamba_layers:
-            #x = layer(self.norm_f(x)) + x
-            x = layer(x) + x
+        for layer in self.layers:
+            x = layer(x)
 
         x = self.norm_f(x)
+        logits = self.lm_head(x)
 
-        return x
+        return logits
 
 
-def exists(val):
-    """
-    Check if the value is not None.
+class ResidualBlock(nn.Module):
+    def __init__(self, config: MambaConfig):
+        """Simple block wrapping Mamba block with normalization and residual connection."""
+        super().__init__()
+        self.config = config
+        self.mixer = MambaBlock(config)
+        self.norm = RMSNorm(config.n_embd)
 
-    Args:
-        val: The value to check.
 
-    Returns:
-        bool: True if value exists (is not None), False otherwise.
-    """
-    return val is not None
+    def forward(self, x):
+        """
+        Args:
+            x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+        Returns:
+            output: shape (b, l, d)
+
+        Official Implementation:
+            Block.forward(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L297
+
+            Note: the official repo chains residual blocks that look like
+                [Add -> Norm -> Mamba] -> [Add -> Norm -> Mamba] -> [Add -> Norm -> Mamba] -> ...
+            where the first Add is a no-op. This is purely for performance reasons as this
+            allows them to fuse the Add->Norm.
+
+            We instead implement our blocks as the more familiar, simpler, and numerically equivalent
+                [Norm -> Mamba -> Add] -> [Norm -> Mamba -> Add] -> [Norm -> Mamba -> Add] -> ....
+
+        """
+        output = self.mixer(self.norm(x)) + x
+
+        return output
+
+
+class MambaBlock(nn.Module):
+    def __init__(self, config: MambaConfig):
+        """A single Mamba block, as described in Figure 3 in Section 3.4 in the Mamba paper [1]."""
+        super().__init__()
+        self.config = config
+
+        self.in_proj = nn.Linear(config.n_embd, config.d_inner * 2, bias=config.bias)
+
+        self.conv1d = nn.Conv1d(
+            in_channels=config.d_inner,
+            out_channels=config.d_inner,
+            bias=config.conv_bias,
+            kernel_size=config.d_conv,
+            groups=config.d_inner,
+            padding=config.d_conv - 1,
+        )
+
+        # x_proj takes in `x` and outputs the input-specific Δ, B, C
+        self.x_proj = nn.Linear(config.d_inner, config.dt_rank + config.d_state * 2, bias=False)
+
+        # dt_proj projects Δ from dt_rank to d_in
+        self.dt_proj = nn.Linear(config.dt_rank, config.d_inner, bias=True)
+
+        A = repeat(torch.arange(1, config.d_state + 1), 'n -> d n', d=config.d_inner)
+        self.A_log = nn.Parameter(torch.log(A))
+        self.D = nn.Parameter(torch.ones(config.d_inner))
+        self.out_proj = nn.Linear(config.d_inner, config.n_embd, bias=config.bias)
+
+
+    def forward(self, x):
+        """Mamba block forward. This looks the same as Figure 3 in Section 3.4 in the Mamba paper [1].
+
+        Args:
+            x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+        Returns:
+            output: shape (b, l, d)
+
+        Official Implementation:
+            class Mamba, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L119
+            mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
+
+        """
+        (b, l, d) = x.shape
+
+        x_and_res = self.in_proj(x)  # shape (b, l, 2 * d_in)
+        (x, res) = x_and_res.split(split_size=[self.args.d_inner, self.args.d_inner], dim=-1)
+
+        x = rearrange(x, 'b l d_in -> b d_in l')
+        x = self.conv1d(x)[:, :, :l]
+        x = rearrange(x, 'b d_in l -> b l d_in')
+
+        x = F.silu(x)
+
+        y = self.ssm(x)
+
+        y = y * F.silu(res)
+
+        output = self.out_proj(y)
+
+        return output
+
+
+    def ssm(self, x):
+        """Runs the SSM. See:
+            - Algorithm 2 in Section 3.2 in the Mamba paper [1]
+            - run_SSM(A, B, C, u) in The Annotated S4 [2]
+
+        Args:
+            x: shape (b, l, d_in)    (See Glossary at top for definitions of b, l, d_in, n...)
+
+        Returns:
+            output: shape (b, l, d_in)
+
+        Official Implementation:
+            mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
+
+        """
+        (d_in, n) = self.A_log.shape
+
+        # Compute ∆ A B C D, the state space parameters.
+        #     A, D are input independent (see Mamba paper [1] Section 3.5.2 "Interpretation of A" for why A isn't selective)
+        #     ∆, B, C are input-dependent (this is a key difference between Mamba and the linear time invariant S4,
+        #                                  and is why Mamba is called **selective** state spaces)
+
+        A = -torch.exp(self.A_log.float())  # shape (d_in, n)
+        D = self.D.float()
+
+        x_dbl = self.x_proj(x)  # (b, l, dt_rank + 2*n)
+
+        (delta, B, C) = x_dbl.split(split_size=[self.args.dt_rank, n, n], dim=-1)  # delta: (b, l, dt_rank). B, C: (b, l, n)
+        delta = F.softplus(self.dt_proj(delta))  # (b, l, d_in)
+
+        y = self.selective_scan(x, delta, A, B, C, D)  # This is similar to run_SSM(A, B, C, u) in The Annotated S4 [2]
+
+        return y
+
+
+    def selective_scan(self, u, delta, A, B, C, D):
+        """Does selective scan algorithm. See:
+            - Section 2 State Space Models in the Mamba paper [1]
+            - Algorithm 2 in Section 3.2 in the Mamba paper [1]
+            - run_SSM(A, B, C, u) in The Annotated S4 [2]
+
+        This is the classic discrete state space formula:
+            x(t + 1) = Ax(t) + Bu(t)
+            y(t)     = Cx(t) + Du(t)
+        except B and C (and the step size delta, which is used for discretization) are dependent on the input x(t).
+
+        Args:
+            u: shape (b, l, d_in)    (See Glossary at top for definitions of b, l, d_in, n...)
+            delta: shape (b, l, d_in)
+            A: shape (d_in, n)
+            B: shape (b, l, n)
+            C: shape (b, l, n)
+            D: shape (d_in,)
+
+        Returns:
+            output: shape (b, l, d_in)
+
+        Official Implementation:
+            selective_scan_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L86
+            Note: I refactored some parts out of `selective_scan_ref` out, so the functionality doesn't match exactly.
+
+        """
+        (b, l, d_in) = u.shape
+        n = A.shape[1]
+
+        # Discretize continuous parameters (A, B)
+        # - A is discretized using zero-order hold (ZOH) discretization (see Section 2 Equation 4 in the Mamba paper [1])
+        # - B is discretized using a simplified Euler discretization instead of ZOH. From a discussion with authors:
+        #   "A is the more important term and the performance doesn't change much with the simplification on B"
+        deltaA = torch.exp(einsum(delta, A, 'b l d_in, d_in n -> b l d_in n'))
+        deltaB_u = einsum(delta, B, u, 'b l d_in, b l n, b l d_in -> b l d_in n')
+
+        # Perform selective scan (see scan_SSM() in The Annotated S4 [2])
+        # Note that the below is sequential, while the official implementation does a much faster parallel scan that
+        # is additionally hardware-aware (like FlashAttention).
+        x = torch.zeros((b, d_in, n), device=deltaA.device)
+        ys = []
+        for i in range(l):
+            x = deltaA[:, i] * x + deltaB_u[:, i]
+            y = einsum(x, C[:, i, :], 'b d_in n, b n -> b d_in')
+            ys.append(y)
+        y = torch.stack(ys, dim=1)  # shape (b, l, d_in)
+
+        y = y + u * D
+
+        return y
 
 
 class RMSNorm(nn.Module):
-    """
-    RMS  Normalization
-
-    Args:
-        dim (int): The dimension of the input.
-        eps (float): The epsilon value.
-
-    Attributes:
-        scale (float): The scale value.
-        gamma (nn.Parameter): The gamma parameter.
-
-    Example:
-        >>> module = RMSNorm(768)
-        >>> x = torch.randn(2, 197, 768)
-        >>> y = module(x)
-        >>> y.shape
-        torch.Size([2, 197, 768])
-
-    """
-
-    def __init__(self, dim):
+    def __init__(self,
+                 d_model: int,
+                 eps: float = 1e-5):
         super().__init__()
-        self.scale = dim**-0.5
-        self.gamma = nn.Parameter(torch.ones(dim))
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(d_model))
+
 
     def forward(self, x):
-        """Forward method implementation."""
-        normed = F.normalize(x, dim=-2)
-        return normed * self.scale * self.gamma
+        output = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
+
+        return output
+
+# class Mamba(nn.Module):
+#     """Mamba model.
+#
+#     Args:
+#         vocab_size (int): The size of the vocabulary.
+#         dim (int): The input dimension.
+#         depth (int): The depth of the Mamba block.
+#         d_state (int): The state dimension. Default is 16.
+#         expand (int): The expansion factor. Default is 2.
+#         dt_rank (Union[int, str]): The rank of the temporal difference (Δ) tensor. Default is "auto".
+#         d_conv (int): The dimension of the convolutional kernel. Default is 4.
+#
+#     Examples:
+#     x = torch.randint(0, 16, (1, 64))
+#     model = Mamba(16, 64, 5, 16)
+#     out = model(x)
+#     print(out)
+#     """
+#
+#     def __init__(
+#             self,
+#             config,
+#     ):
+#         """Full Mamba model."""
+#         super(Mamba, self).__init__()
+#         assert config.vocab_size is not None
+#         assert config.block_size is not None
+#
+#         self.norm_f = RMSNorm(config.n_embd)
+#         self.mamba_layers = nn.ModuleList(
+#             [
+#                 torch.nn.Sequential(
+#                     MambaBlock(dim=config.n_embd, depth=config.n_layer, d_state=config.d_state, d_conv=config.d_conv),
+#                     RMSNorm(config.n_embd)
+#                 )
+#                 for _ in range(config.n_layer)
+#             ]
+#         )
+#         # Projection for img
+#         self.img_proj = nn.Linear(config.img_dim, config.n_embd)
+#         print("number of parameters: %.2fM" % (self.get_num_params() / 1e6,))
+#
+#     def get_num_params(self, non_embedding=True):
+#         """
+#         Return the number of parameters in the model.
+#         """
+#         n_params = sum(p.numel() for p in self.parameters())
+#         return n_params
+#
+#     def forward(self, x: torch.Tensor, context: torch.Tensor = None):
+#         """
+#         Args:
+#             x (float tensor): input tensor
+#
+#         """
+#
+#         if exists(context):
+#             # Project the image
+#             projected_img = self.img_proj(context)
+#
+#             # Concatenate the image and text
+#             x = torch.cat([x, projected_img], dim=1)
+#
+#         for layer in self.mamba_layers:
+#             #x = layer(self.norm_f(x)) + x
+#             x = layer(x) + x
+#
+#         x = self.norm_f(x)
+#
+#         return x
+#
+#
+# def exists(val):
+#     """
+#     Check if the value is not None.
+#
+#     Args:
+#         val: The value to check.
+#
+#     Returns:
+#         bool: True if value exists (is not None), False otherwise.
+#     """
+#     return val is not None
+#
+#
+# class RMSNorm(nn.Module):
+#     """
+#     RMS  Normalization
+#
+#     Args:
+#         dim (int): The dimension of the input.
+#         eps (float): The epsilon value.
+#
+#     Attributes:
+#         scale (float): The scale value.
+#         gamma (nn.Parameter): The gamma parameter.
+#
+#     Example:
+#         >>> module = RMSNorm(768)
+#         >>> x = torch.randn(2, 197, 768)
+#         >>> y = module(x)
+#         >>> y.shape
+#         torch.Size([2, 197, 768])
+#
+#     """
+#
+#     def __init__(self, dim):
+#         super().__init__()
+#         self.scale = dim**-0.5
+#         self.gamma = nn.Parameter(torch.ones(dim))
+#
+#     def forward(self, x):
+#         """Forward method implementation."""
+#         normed = F.normalize(x, dim=-2)
+#         return normed * self.scale * self.gamma
